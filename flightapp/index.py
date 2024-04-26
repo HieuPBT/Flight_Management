9b332426b66820a4bb9444dccd167601efe2c4b3
Hiếu <hieu@192.168.1.148>
2024-04-26 22:04:46 +0700

    add main2

--- flightapp/index.py
+++ flightapp/index.py
@@ -4,6 +4,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/search_ticket')
+def search_ticket():
+    return render_template('ticket.html')
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
