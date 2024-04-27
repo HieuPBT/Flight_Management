324edc441b978ebbd82c9d3c312fb574f5717405
lokhue <2151013052loc@gmail.com>
2024-04-27 18:16:51 +0700

    add model ThongTinNguoiDung and relationship with User model

--- flightapp/models.py
+++ flightapp/models.py
@@ -23,16 +23,19 @@ class Base(db.Model):
 class User(Base, UserMixin):
     username = Column(String(50), nullable=False, unique=True)
     password = Column(String(50), nullable=False)
+    user_role = Column(Enum(UserRole), default=UserRole.USER)
+    thong_tin_nguoi_dung = relationship('ThongTinNguoiDung', backref='user', lazy=False, uselist=False)
+    quy_dinh = relationship('QuyDinh', foreign_keys='QuyDinh.nhan_vien_quan_tri_id', backref='nhan_vien_quan_tri', lazy=True)
+
+
+class ThongTinNguoiDung(Base):
     ho_va_ten = Column(String(50), nullable=False)
     so_dien_thoai = Column(String(20))
     dia_chi = Column(String(255))
     email = Column(String(50))
     CCCD = Column(String(20))
-    user_role = Column(Enum(UserRole), default=UserRole.USER)
-
-    ve_mua = relationship('Ve', foreign_keys='Ve.khach_hang_id', backref='khach_hang', lazy=True)
-    ve_ban = relationship('Ve', foreign_keys='Ve.nhan_vien_ban_ve_id', backref='nhan_vien_ban_ve', lazy=True)
-    quy_dinh = relationship('QuyDinh', foreign_keys='QuyDinh.nhan_vien_quan_tri_id', backref='nhan_vien_quan_tri', lazy=True)
+    tai_khoan_id = Column(Integer, ForeignKey('user.id'), nullable=True)
+    ve = relationship('Ve', foreign_keys='Ve.khach_hang_id', backref='thong_tin_nguoi_dung', lazy=True)
 
 
 class SanBay(Base):
@@ -66,7 +69,7 @@ class HangVe(Base):
 
 
 class ChuyenBay(Base):
-    ngay_khoi_hanh = Column(DateTime, nullable=False)
+    ngay_gio_khoi_hanh = Column(DateTime, nullable=False)
     thoi_gian_bay = Column(Integer, nullable=False)
     tuyen_bay_id = Column(Integer, ForeignKey('tuyen_bay.id'), nullable=False)
     nhan_vien_quan_tri_id = Column(Integer, ForeignKey('user.id'), nullable=False)
@@ -97,9 +100,8 @@ class GheMayBay(Base):
 class Ve(Base):
     ghe_may_bay_id = Column(Integer, ForeignKey('ghe_may_bay.id'), nullable=False)
     hang_ve_chuyen_bay_id = Column(Integer, ForeignKey('hang_ve_chuyen_bay.id'), nullable=False)
-    khach_hang_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    khach_hang_id = Column(Integer, ForeignKey('thong_tin_nguoi_dung.id'), nullable=False)
     hoa_don_id = Column(Integer, ForeignKey('hoa_don.id'), nullable=False)
-    nhan_vien_ban_ve_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 
 class HoaDon(Base):
@@ -123,25 +125,34 @@ class QuyDinh(Base):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        sb1 = SanBay(ten='Cà Mau', tinh='Cà Mau')
-        sb2 = SanBay(ten='Cần Thơ', tinh='Cần Thơ')
-        sb3 = SanBay(ten='Đà Nẵng', tinh='Đà Nẵng')
-        sb4 = SanBay(ten='Pleiku', tinh='Pleiku')
-        sb5 = SanBay(ten='Tân Sơn Nhất', tinh='Thành phố Hồ Chí Minh')
-        sb6 = SanBay(ten='Nội Bài', tinh='Hà Nội')
-        sb7 = SanBay(ten='Điện Biên Phủ', tinh='Điện Biên')
-        sb8 = SanBay(ten='Liên Khương', tinh='Lâm Đồng')
-        sb9 = SanBay(ten='Phú Bài', tinh='Thừa Thiên Huế')
-        sb10 = SanBay(ten='Vinh', tinh='Nghệ An')
-
-        db.session.add_all([sb1, sb2, sb3, sb4, sb5, sb6, sb7, sb8, sb9, sb10])
-
+        # sb1 = SanBay(ten='Cà Mau', tinh='Cà Mau')
+        # sb2 = SanBay(ten='Cần Thơ', tinh='Cần Thơ')
+        # sb3 = SanBay(ten='Đà Nẵng', tinh='Đà Nẵng')
+        # sb4 = SanBay(ten='Pleiku', tinh='Pleiku')
+        # sb5 = SanBay(ten='Tân Sơn Nhất', tinh='Thành phố Hồ Chí Minh')
+        # sb6 = SanBay(ten='Nội Bài', tinh='Hà Nội')
+        # sb7 = SanBay(ten='Điện Biên Phủ', tinh='Điện Biên')
+        # sb8 = SanBay(ten='Liên Khương', tinh='Lâm Đồng')
+        # sb9 = SanBay(ten='Phú Bài', tinh='Thừa Thiên Huế')
+        # sb10 = SanBay(ten='Vinh', tinh='Nghệ An')
+        #
+        # db.session.add_all([sb1, sb2, sb3, sb4, sb5, sb6, sb7, sb8, sb9, sb10])
+        #
         # db.session.commit()
         #
-        import hashlib
-        u = User(username='admin',
-                 password=str(hashlib.md5("123456".encode('utf-8')).hexdigest()),
-                 ho_va_ten = 'admin',
-                 user_role=UserRole.ADMIN)
-        db.session.add(u)
-        db.session.commit()
+        # import hashlib
+        #
+        #
+        # u = User(username='admin',
+        #          password=str(hashlib.md5("123456".encode('utf-8')).hexdigest()),
+        #          user_role=UserRole.ADMIN)  # Use the ID of the created record
+        # db.session.add(u)
+        # db.session.commit()
+
+        # info = ThongTinNguoiDung(ho_va_ten="admin", tai_khoan_id=u.id)
+        # db.session.add(info)  # Add and commit information first
+        # db.session.commit()
+
+        # info2 = ThongTinNguoiDung(ho_va_ten="Nguyễn Xuân Lộc", so_dien_thoai='0362655091', dia_chi='Gia Lai', email='2151013052loc@gmail.com', CCCD='0798723983792')
+        # db.session.add(info2)  # Add and commit information first
+        # db.session.commit()
