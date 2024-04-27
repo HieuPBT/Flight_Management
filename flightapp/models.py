b729c17684f2e950e14291020828c88f8a1ec98b
lokhue <2151013052loc@gmail.com>
2024-04-27 14:52:25 +0700

    update models

--- flightapp/models.py
+++ flightapp/models.py
@@ -6,6 +6,12 @@ from enum import Enum as RoleEnum
 from datetime import datetime
 
 
+class UserRole(RoleEnum):
+    USER = 1
+    ADMIN = 2
+    TICKET_SELLER = 3
+
+
 class Base(db.Model):
     __abstract__ = True
 
@@ -14,7 +20,7 @@ class Base(db.Model):
     created_date = Column(DateTime, default=datetime.now())
 
 
-class KhachHang(Base, UserMixin):
+class User(Base, UserMixin):
     username = Column(String(50), nullable=False, unique=True)
     password = Column(String(50), nullable=False)
     ho_va_ten = Column(String(50), nullable=False)
@@ -22,22 +28,18 @@ class KhachHang(Base, UserMixin):
     dia_chi = Column(String(255))
     email = Column(String(50))
     CCCD = Column(String(20))
-    ve = relationship('Ve', backref='khach_hang', lazy=True)
+    user_role = Column(Enum(UserRole), default=UserRole.USER)
 
-
-class NhanVienQuanTri(KhachHang):
-    chuyen_bay = relationship('ChuyenBay', backref='nhan_vien_quan_tri', lazy=True)
-
-
-class NhanVienBanVe(KhachHang):
-    ve = relationship('Ve', backref='nhan_vien_ban_ve', lazy=True)
+    ve_mua = relationship('Ve', foreign_keys='Ve.khach_hang_id', backref='khach_hang', lazy=True)
+    ve_ban = relationship('Ve', foreign_keys='Ve.nhan_vien_ban_ve_id', backref='nhan_vien_ban_ve', lazy=True)
+    quy_dinh = relationship('QuyDinh', foreign_keys='QuyDinh.nhan_vien_quan_tri_id', backref='nhan_vien_quan_tri', lazy=True)
 
 
 class SanBay(Base):
-    ten = Column(String(20), nullable=False)
-    thanh_pho = Column(String(20), nullable=False)
-    tuyen_bay_di = relationship('TuyenBay', foreign_keys='TuyenBay.san_bay_di_id', backref='san_bay', lazy=True)
-    tuyen_bay_den = relationship('TuyenBay', foreign_keys='TuyenBay.san_bay_den_id', backref='san_bay', lazy=True)
+    ten = Column(String(50), nullable=False)
+    tinh = Column(String(100), nullable=False)
+    tuyen_bay_di = relationship('TuyenBay', foreign_keys='TuyenBay.san_bay_di_id', backref='san_bay_di', lazy=True)
+    tuyen_bay_den = relationship('TuyenBay', foreign_keys='TuyenBay.san_bay_den_id', backref='san_bay_den', lazy=True)
     san_bay_trung_gian = relationship('SanBayTrungGian', backref='san_bay', lazy=True)
 
 
@@ -67,7 +69,7 @@ class ChuyenBay(Base):
     ngay_khoi_hanh = Column(DateTime, nullable=False)
     thoi_gian_bay = Column(Integer, nullable=False)
     tuyen_bay_id = Column(Integer, ForeignKey('tuyen_bay.id'), nullable=False)
-    nhan_vien_quan_tri_id = Column(Integer, ForeignKey('nhan_vien_quan_tri.id'), nullable=False)
+    nhan_vien_quan_tri_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     may_bay_id = Column(Integer, ForeignKey('may_bay.id'), nullable=False)
     san_bay_trung_gian = relationship('SanBayTrungGian', backref='chuyen_bay', lazy=True)
 
@@ -95,9 +97,9 @@ class GheMayBay(Base):
 class Ve(Base):
     ghe_may_bay_id = Column(Integer, ForeignKey('ghe_may_bay.id'), nullable=False)
     hang_ve_chuyen_bay_id = Column(Integer, ForeignKey('hang_ve_chuyen_bay.id'), nullable=False)
-    khach_hang_id = Column(Integer, ForeignKey('khach_hang.id'), nullable=False)
+    khach_hang_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     hoa_don_id = Column(Integer, ForeignKey('hoa_don.id'), nullable=False)
-    nhan_vien_ban_ve_id = Column(Integer, ForeignKey('nhan_vien_ban_ve.id'), nullable=False)
+    nhan_vien_ban_ve_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 
 class HoaDon(Base):
@@ -112,9 +114,34 @@ class SanBayTrungGian(Base):
     )
 
 
-
+class QuyDinh(Base):
+    key = Column(String(50), nullable=False, unique=True)
+    value = Column(Integer, nullable=False)
+    nhan_vien_quan_tri_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        sb1 = SanBay(ten='Cà Mau', tinh='Cà Mau')
+        sb2 = SanBay(ten='Cần Thơ', tinh='Cần Thơ')
+        sb3 = SanBay(ten='Đà Nẵng', tinh='Đà Nẵng')
+        sb4 = SanBay(ten='Pleiku', tinh='Pleiku')
+        sb5 = SanBay(ten='Tân Sơn Nhất', tinh='Thành phố Hồ Chí Minh')
+        sb6 = SanBay(ten='Nội Bài', tinh='Hà Nội')
+        sb7 = SanBay(ten='Điện Biên Phủ', tinh='Điện Biên')
+        sb8 = SanBay(ten='Liên Khương', tinh='Lâm Đồng')
+        sb9 = SanBay(ten='Phú Bài', tinh='Thừa Thiên Huế')
+        sb10 = SanBay(ten='Vinh', tinh='Nghệ An')
+
+        db.session.add_all([sb1, sb2, sb3, sb4, sb5, sb6, sb7, sb8, sb9, sb10])
+
+        # db.session.commit()
+        #
+        import hashlib
+        u = User(username='admin',
+                 password=str(hashlib.md5("123456".encode('utf-8')).hexdigest()),
+                 ho_va_ten = 'admin',
+                 user_role=UserRole.ADMIN)
+        db.session.add(u)
+        db.session.commit()
